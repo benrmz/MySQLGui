2c290ed7f39d97c4648925d99919c2c7292e24c5
benji-b-rmz <benji.b.ramirez@gmail.com>
2017-03-07 19:35:13 -0800

    Update DBConnect.py

--- DBConnect.py
+++ DBConnect.py
@@ -2,15 +2,6 @@
 #November 23 2016
 import pymysql
 from pymysql import *
-# conn= pymysql.connect(host='localhost',
-#                       user='root',
-#                       password='skate805',
-#                       db='aerodb',
-#                       charset='utf8mb4',
-#                       cursorclass=pymysql.cursors.DictCursor)
-# a=conn.cursor()
-# sql='CREATE TABLE `users` (`id` int(11) NOT NULL AUTO_INCREMENT,`email` varchar(255) NOT NULL,`password` varchar(255) NOT NULL,PRIMARY KEY (`id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;'
-# a.execute(sql)
 
 class MyDB():
     def __init__(self, host, user, password, database):
